2519984650ea29ddec4c806b64bfea9da75fa2ec
011bq-001 <011bq-001@011bq-001s-MacBook-Air.local>
2023-04-27 22:07:32 +0530

    Added slices

---
+++ src/store/slices/carsSlice.js
@@ -0,0 +1,35 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const carsSlice = createSlice({
+  name: "cars",
+  initialState: {
+    searchTerm: "",
+    cars: [],
+  },
+  reducers: {
+    changeSearchTerm(state, action) {
+      state.searchTerm = action.payload;
+    },
+    addCar(state, action) {
+      state.cars.push({
+      id: nanoid(), 
+      name: action.payload.name, 
+      cost: action.payload.cost 
+    });
+    },
+    removeCar(state, action) {
+      const updated = state.cars.filter((car) => {
+        return car.id !== action.payload
+      });
+      state.cars = updated;
+    },
+  },
+});
+
+export const {
+  changeSearchTerm,
+  addCar,
+  removeCar
+} = carsSlice.actions;
+
+export const carsReducer = carsSlice.reducer;
