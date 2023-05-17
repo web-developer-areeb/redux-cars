c83b154a475d789ad126757d4ca9dddff00d1530
011bq-001 <011bq-001@192.168.1.2>
2023-05-17 10:55:29 +0530

    Added Form.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React from 'react';
+import 'bulma/css/bulma.css';
+import './styles.css';
 import { createRoot } from 'react-dom/client';
-
+import { Provider } from 'react-redux';
+import { store } from './store';
 import App from "./App";
 
 const el = document.getElementById('root');
 const root = createRoot(el);
 
-root.render(<App />);
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
 
 
