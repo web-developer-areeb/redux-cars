d1b031b63fb88ccf44f4acd3c80c5573eea017fb
011bq-001 <011bq-001@011bq-001s-MacBook-Air.local>
2023-05-19 00:30:15 +0530

    Highlight name if match.

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -5,24 +5,32 @@ import { removeCar } from "../store";
 function CarList() {
   const dispatch = useDispatch();
 
-  const cars = useSelector(({cars: {data, searchTerm}}) => {
-    return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+  const { cars, name } = useSelector(({form, cars: {data, searchTerm}}) => {
+    const filteredCars =  data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
+
+    return {
+      cars: filteredCars,
+      name: form.name
+    }
   });
 
   const handleCarDelete = (car) => {
     dispatch(removeCar(car.id));
   };
 
-  const renderedCars = cars.map((car) => (
-    <div key={car.id} className="panel">
-      <p>
-        {car.name} -${car.cost}
-      </p>
-      <button className="button is-danger" onClick={() => handleCarDelete(car)}>
-        Delete
-      </button>
-    </div>
-  ));
+  const renderedCars = cars.map((car) => {
+    const bold = name && car.name.toLowerCase().includes(name.toLowerCase());
+    return (
+      <div key={car.id} className={`panel ${bold && 'bold'}`}>
+        <p>
+          {car.name} -${car.cost}
+        </p>
+        <button className="button is-danger" onClick={() => handleCarDelete(car)}>
+          Delete
+        </button>
+      </div>
+    )
+  });
 
   return (
     <div className="car-list">
