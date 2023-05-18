6631871f9c46b6689d344c681533cdafd5112fa2
011bq-001 <011bq-001@011bq-001s-MacBook-Air.local>
2023-05-18 10:37:13 +0530

    Added car list. Added car search functionality.

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -5,8 +5,8 @@ import { removeCar } from "../store";
 function CarList() {
   const dispatch = useDispatch();
 
-  const cars = useSelector((state) => {
-    return state.cars.data;
+  const cars = useSelector(({cars: {data, searchTerm}}) => {
+    return data.filter((car) => car.name.toLowerCase().includes(searchTerm.toLowerCase()));
   });
 
   const handleCarDelete = (car) => {
